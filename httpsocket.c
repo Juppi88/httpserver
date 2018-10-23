ef7c1c832e2919f8ea9c6a0968e940c6a036b201
Tuomo Jauhiainen <juppi@project-apollo.co.uk>
2018-10-23 22:23:03 +0300

    Fix attempt #2.

--- httpsocket.c
+++ httpsocket.c
@@ -19,7 +19,31 @@ void http_socket_set_non_blocking(socket_t sock)
 	ioctlsocket(sock, FIONBIO, &mode);
 }
 
+int http_socket_write_all(socket_t sock, const void *buffer, size_t length)
+{
+	const char *p = buffer;
+	ssize_t sent;
+
+	while (length > 0) {
+
+		sent = write(sock, p, length);
+
+		if (sent <= 0) {
+			return -1;
+		}
+
+		// Successful send.
+		p += sent;
+		length -= sent;
+	}
+
+	return 0;
+}
+
 #else
+
+#include <errno.h>
+#include <time.h>
 
 void http_socket_initialize(void) {}
 void http_socket_shutdown(void) {}
@@ -36,8 +60,6 @@ void http_socket_set_non_blocking(socket_t sock)
 	fcntl(sock, F_SETFL, flags);
 }
 
-#endif
-
 int http_socket_write_all(socket_t sock, const void *buffer, size_t length)
 {
 	const char *p = buffer;
@@ -47,13 +69,32 @@ int http_socket_write_all(socket_t sock, const void *buffer, size_t length)
 
 		sent = write(sock, p, length);
 
-		if (sent <= 0) {
+		if (sent < 0) {
+
+			if (errno != EAGAIN) {
+				return -1;
+			}
+
+			// send error, select and try again.
+			struct timeval tv = { .tv_sec = 1 };
+
+			fd_set fds;
+			FD_ZERO(&fds);
+			FD_SET(0, &fds);
+
+			if (select(sock + 1, NULL, &fds, NULL, &tv)) {}
+		}
+		else if (sent == 0) {
 			return -1;
 		}
-
-		p += sent;
-		length -= sent;
+		else {
+			// Successful send.
+			p += sent;
+			length -= sent;
+		}
 	}
 
 	return 0;
 }
+
+#endif
